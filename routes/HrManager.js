d39a209224b7d44eeaa906441c0a6d50b141e608
samir <harchesamir007@gmail.com>
2023-03-30 23:18:11 +0100

    search among request

--- routes/HrManager.js
+++ routes/HrManager.js
@@ -58,24 +58,9 @@ const pool = mysql.createPool(
       res.status(500).send('Server Error');
     }
   });
-  //search among requests
-  router.get('/search', async (req, res) => {
-    try {
-      const query = req.query.for;
-      const connection = await pool.getConnection();
-      const sqlQuery = `
-        SELECT * FROM proofs
-        WHERE image_url LIKE '%${query}%'
-        OR request_id LIKE '%${query}%'
-      `;
-      const result = await connection.query(sqlQuery);
-      res.json(result[0]);
-      connection.release();
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal server error');
-    }
-  });
+ 
+
+
 //search among accounts
   router.get('/searchAccounts', async (req, res) => {
     try {
@@ -201,7 +186,56 @@ const pool = mysql.createPool(
 
 
 
+ router.get('/searchRequests', async (req, res) => {
+    try {
+      const query = req.query.for;
+      const connection = await pool.getConnection();
+      const sqlQuery = `
+      SELECT COUNT(*) as total FROM requests
+        WHERE description LIKE '%${query}%'
+        OR requestedBy LIKE '%${query}%'
+      `;
+      //const result = await connection.query(sqlQuery);
+      //res.json(result[0]);
 
+      const [result] = await connection.query(sqlQuery);
+     
+      const totalRecords = result[0].total;
+    
+  
+      // Calculate pagination info
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+      const totalPages = Math.ceil(totalRecords / limit);
+      const offset = (page - 1) * limit;
+      const [records] = await connection.query(`SELECT * FROM requests WHERE description LIKE '%${query}%' OR requestedBy LIKE '%${query}%' ORDER BY createdAt LIMIT ?, ?`, [offset, limit]);
+      let previousPage = null;
+      let nextPage = null;
+      if (page > 1) {
+        previousPage = page - 1;
+      }
+      if (page < totalPages) {
+        nextPage = page + 1;
+      }
+  
+      const infos = {
+        previous: previousPage,
+        next: nextPage,
+        totalPages,
+        currentPage: page,
+        totalRecords : totalRecords
+      };
+      res.send({infos, records})
+      connection.release();
+
+
+
+      
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    }
+  });
 
 
 
