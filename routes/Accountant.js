00ed1188224cd0841d608273e879eb428c1e2dec
samir <harchesamir007@gmail.com>
2023-05-20 11:47:04 +0100

    a

--- routes/Accountant.js
+++ routes/Accountant.js
@@ -321,6 +321,53 @@ router.get('/budget', async (req, res) => {
 
 
 
+router.get('/transactions', async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
 
+    // Get total number of records
+    const [result] = await connection.query('SELECT COUNT(*) as total FROM transactions');
+   
+    const totalRecords = result[0].total;
+
+    // Calculate pagination info
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const totalPages = Math.ceil(totalRecords / limit);
+    const offset = (page - 1) * limit;
+
+    // Get records for the requested page
+    const [records] = await connection.query('SELECT t.*, r.* FROM transactions AS t LEFT JOIN requests AS r ON t.requests = r.id ORDER BY t.createdAt LIMIT ?, ?', [offset, limit]);
+
+    // Calculate previous and next page numbers
+    let previousPage = null;
+    let nextPage = null;
+    if (page > 1) {
+      previousPage = page - 1;
+    }
+    if (page < totalPages) {
+      nextPage = page + 1;
+    }
+
+    const infos = {
+      previous: previousPage,
+      next: nextPage,
+      totalPages,
+      currentPage: page,
+      totalRecords: totalRecords,
+
+    };
+
+    res.json({
+      infos,
+      records,
+    });
+
+    connection.release();
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
   
 module.exports = router;
