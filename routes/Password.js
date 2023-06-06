581092d55fdadf7544d1fc8d9ccc287d439194ac
samir <harchesamir007@gmail.com>
2023-06-06 22:41:49 +0100

    a

--- routes/Password.js
+++ routes/Password.js
@@ -53,7 +53,7 @@ const pool = require('../db')
         from: 'esi4benefits@gmail.com',
         to: email,
         subject: 'Reset Your Password',
-        text: `Click this link to reset your password: https://server-social-benefits.vercel.app/ResetPassword?token=${token}&email=${email}`,
+        text: `Click this link to reset your password: https://social-benefits-esi.vercel.app//ResetPassword?token=${token}&email=${email}`,
       };
   
       await transporter.sendMail(mailOptions);
